f750bccfe2615ef29372880e2277576d0c878f69
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-10-08 11:05:17 +0200

    Gestione del comportamento del form con chiamata POST (PROVA)

--- src/components/MyBlog.jsx
+++ src/components/MyBlog.jsx
@@ -1,5 +1,21 @@
-export default function MyBlog () {
+import {useState} from "react";
+
+export default function MyBlog () { {/*Inseriamo tutti i campi che vogliamo nel form*/}
+    const [form, setForm] = useState({ 
+        author: "",
+        title: "",
+        body: "",
+        public: false 
+    });
+
+    const submitForm = (e) => { {/*Gestione dei dati d'entrata del form: preveniamo il comportamento di default del subit e inviamo i dati al server di POST tramite chiamata*/}
+        e.preventDefault();
+        axios.post("https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts", form)
+        .then (res => console.log (res))
+        .catch (error => console.log (error))
+    }; {/*Se la chiamata va a buon fine, stampiamo il risultato del server su console. Al contrario, stamperà un errore*/}
+
     return () => {
-        
+
     }
 }
